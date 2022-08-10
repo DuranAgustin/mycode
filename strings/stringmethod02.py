e46772efa378f6f607c7f1f23de0328a0a23ef10
DuranAgustin <DuranAgustin@users.noreply.github.com>
2022-08-10 22:03:43 +0000

    join and plsit

---
+++ strings/stringmethod02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def main():
+    lilstring = "alta3 reasearch offers classes on python coding"
+    newlist = lilstring.split(" ")
+    print(newlist)
+
+    myiplist = ["192", "0", "0", "10"]
+
+    singleip = ".".join(myiplist)
+    print(singleip)
+main()
+
