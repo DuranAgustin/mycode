7e1bdbb3e193c533def52c2b753d8ccc11492e99
DuranAgustin <DuranAgustin@users.noreply.github.com>
2022-08-17 16:36:46 +0000

    pullilng in data from API with python standard library

---
+++ iss/requests-ride_iss.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""tracking the iss using
+   api.open-notify.org/astros.json | Alta3 Research"""
+
+# notice we no longer need to import urllib.request or json
+import urllib.request
+import json
+## Define URL
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+def main():
+    """runtime code"""
+
+    ## Call the webservice
+    groundctrl = urllib.request.urlopen(MAJORTOM)
+    # send a post with requests.post()
+    # send a put with requests.put()
+    # send a delete with requests.delete()
+    # send a head with requests.head()
+
+    ## strip the json off the 200 that was returned by our API
+    ## translate the json into python lists and dictionaries
+    helmet = groundctrl.read()
+    helmetson = json.loads(helmet.decode("utf-8"))
+
+    print("People in space: " + str(helmetson["number"]))
+
+    # display every item in a list
+    for astro in helmetson["people"]:
+        # display ONLY the name value associated with astro
+        print(astro["name"] + " on the " + astro["craft"])
+
+
+if __name__ == "__main__":
+    main()
+
